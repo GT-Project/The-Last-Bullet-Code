9563b92535d62d23194a3890ba9d3b616fb99735
rzkid <rzkid@bitbucket.org>
2018-07-24 11:42:42 +0300

    modified:   sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp 	modified:   sp/src/game/server/server_episodic.vcxproj.filters 	new file:   sp/src/game/server/weapon_c96.cpp 	modified:   sp/src/lib/public/mathlib.lib 	modified:   sp/src/lib/public/tier1.lib

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -42,6 +42,7 @@ STUB_WEAPON_CLASS(weapon_mp40, WeaponMp40, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_minigun, WeaponMinigun, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_k98, Weaponk98, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_knife, WeaponKnife, C_BaseHLBludgeonWeapon);
+STUB_WEAPON_CLASS(weapon_c96, WeaponC96, C_BaseHLCombatWeapon);
 
 #ifdef HL2_EPISODIC
 STUB_WEAPON_CLASS( weapon_hopwire, WeaponHopwire, C_BaseHLCombatWeapon );
---
+++ sp/src/game/server/weapon_c96.cpp
@@ -0,0 +1,362 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "basehlcombatweapon.h"
+#include "npcevent.h"
+#include "basecombatcharacter.h"
+#include "ai_basenpc.h"
+#include "player.h"
+#include "game.h"
+#include "in_buttons.h"
+#include "grenade_ar2.h"
+#include "ai_memory.h"
+#include "soundent.h"
+#include "rumble_shared.h"
+#include "gamestats.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CWeaponC96 : public CHLMachineGun
+{
+	DECLARE_DATADESC();
+public:
+	DECLARE_CLASS(CWeaponC96, CHLMachineGun);
+
+	CWeaponC96();
+
+	DECLARE_SERVERCLASS();
+
+	void	Precache(void);
+	void	AddViewKick(void);
+	void	DrawHitmarker(void);
+	void	PrimaryAttack(void);
+	//	void	SecondaryAttack( void );
+
+	int		GetMinBurst() { return 2; }
+	int		GetMaxBurst() { return 5; }
+
+	virtual void Equip(CBaseCombatCharacter *pOwner);
+	bool	Reload(void);
+
+	float	GetFireRate(void) { return 0.6f; }	// 13.3hz
+	int		CapabilitiesGet(void) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+	//	int		WeaponRangeAttack2Condition( float flDot, float flDist );
+	Activity	GetPrimaryAttackActivity(void);
+
+	virtual const Vector& GetBulletSpread(void)
+	{
+		static const Vector cone = VECTOR_CONE_3DEGREES;
+		return cone;
+	}
+
+	const WeaponProficiencyInfo_t *GetProficiencyValues();
+
+	void FireNPCPrimaryAttack(CBaseCombatCharacter *pOperator, Vector &vecShootOrigin, Vector &vecShootDir);
+	//	void Operator_ForceNPCFire( CBaseCombatCharacter  *pOperator, bool bSecondary );
+	void Operator_HandleAnimEvent(animevent_t *pEvent, CBaseCombatCharacter *pOperator);
+
+	DECLARE_ACTTABLE();
+
+protected:
+
+	Vector	m_vecTossVelocity;
+	float	m_flNextGrenadeCheck;
+};
+
+IMPLEMENT_SERVERCLASS_ST(CWeaponC96, DT_WeaponC96)
+END_SEND_TABLE()
+
+LINK_ENTITY_TO_CLASS(weapon_c96, CWeaponC96);
+PRECACHE_WEAPON_REGISTER(weapon_c96);
+
+BEGIN_DATADESC(CWeaponC96)
+
+DEFINE_FIELD(m_vecTossVelocity, FIELD_VECTOR),
+DEFINE_FIELD(m_flNextGrenadeCheck, FIELD_TIME),
+
+END_DATADESC()
+
+acttable_t	CWeaponC96::m_acttable[] =
+{
+	{ ACT_IDLE, ACT_IDLE_PISTOL, true },
+	{ ACT_IDLE_ANGRY, ACT_IDLE_ANGRY_PISTOL, true },
+	{ ACT_RANGE_ATTACK1, ACT_RANGE_ATTACK_PISTOL, true },
+	{ ACT_RELOAD, ACT_RELOAD_PISTOL, true },
+	{ ACT_WALK_AIM, ACT_WALK_AIM_PISTOL, true },
+	{ ACT_RUN_AIM, ACT_RUN_AIM_PISTOL, true },
+	{ ACT_GESTURE_RANGE_ATTACK1, ACT_GESTURE_RANGE_ATTACK_PISTOL, true },
+	{ ACT_RELOAD_LOW, ACT_RELOAD_PISTOL_LOW, false },
+	{ ACT_RANGE_ATTACK1_LOW, ACT_RANGE_ATTACK_PISTOL_LOW, false },
+	{ ACT_COVER_LOW, ACT_COVER_PISTOL_LOW, false },
+	{ ACT_RANGE_AIM_LOW, ACT_RANGE_AIM_PISTOL_LOW, false },
+	{ ACT_GESTURE_RELOAD, ACT_GESTURE_RELOAD_PISTOL, false },
+	{ ACT_WALK, ACT_WALK_PISTOL, false },
+	{ ACT_RUN, ACT_RUN_PISTOL, false },
+};
+
+IMPLEMENT_ACTTABLE(CWeaponC96);
+
+//=========================================================
+CWeaponC96::CWeaponC96()
+{
+	m_fMinRange1 = 0;// No minimum range. 
+	m_fMaxRange1 = 1400;
+
+	m_bAltFiresUnderwater = false;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponC96::Precache(void)
+{
+	UTIL_PrecacheOther("grenade_ar2");
+
+	BaseClass::Precache();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Give this weapon longer range when wielded by an ally NPC.
+//-----------------------------------------------------------------------------
+void CWeaponC96::Equip(CBaseCombatCharacter *pOwner)
+{
+	if (pOwner->Classify() == CLASS_PLAYER_ALLY)
+	{
+		m_fMaxRange1 = 3000;
+	}
+	else
+	{
+		m_fMaxRange1 = 1400;
+	}
+
+	BaseClass::Equip(pOwner);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponC96::FireNPCPrimaryAttack(CBaseCombatCharacter *pOperator, Vector &vecShootOrigin, Vector &vecShootDir)
+{
+	// FIXME: use the returned number of bullets to account for >10hz firerate
+	WeaponSoundRealtime(SINGLE_NPC);
+
+	CSoundEnt::InsertSound(SOUND_COMBAT | SOUND_CONTEXT_GUNFIRE, pOperator->GetAbsOrigin(), SOUNDENT_VOLUME_MACHINEGUN, 0.2, pOperator, SOUNDENT_CHANNEL_WEAPON, pOperator->GetEnemy());
+	pOperator->FireBullets(1, vecShootOrigin, vecShootDir, VECTOR_CONE_PRECALCULATED,
+		MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 2, entindex(), 0);
+
+	pOperator->DoMuzzleFlash();
+	m_iClip1 = m_iClip1 - 1;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+
+void CWeaponC96::DrawHitmarker(void)
+{
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+
+	if (pPlayer == NULL)
+	{
+		return;
+	}
+
+#ifndef CLIENT_DLL
+	CSingleUserRecipientFilter filter(pPlayer);
+	UserMessageBegin(filter, "ShowHitmarker");
+	WRITE_BYTE(1);
+	MessageEnd();
+#endif
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+
+void CWeaponC96::PrimaryAttack(void)
+{
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+	if (!pPlayer)
+	{
+		return;
+	}
+
+	BaseClass::PrimaryAttack();
+
+	m_iPrimaryAttacks++;
+	// set up the vectors and traceline
+	trace_t tr;
+	Vector	vecStart, vecStop, vecDir;
+
+	// get the angles
+	AngleVectors(pPlayer->EyeAngles(), &vecDir);
+
+	// get the vectors
+	vecStart = pPlayer->Weapon_ShootPosition();
+	vecStop = vecStart + vecDir * MAX_TRACE_LENGTH;
+
+	// do the traceline
+	UTIL_TraceLine(vecStart, vecStop, MASK_ALL, pPlayer, COLLISION_GROUP_NONE, &tr);
+
+	// check to see if we hit an NPC
+	if (tr.m_pEnt)
+	{
+		if (tr.m_pEnt->IsNPC())
+		{
+#ifndef CLIENT_DLL		// Light Kill : Draw ONLY if we hit enemy
+			if (pPlayer->GetDefaultRelationshipDisposition(tr.m_pEnt->Classify()) != D_HT)
+			{
+				//DevMsg("Neitral npc ! \n");
+			}
+			else
+				DrawHitmarker();
+#endif
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponC96::Operator_HandleAnimEvent(animevent_t *pEvent, CBaseCombatCharacter *pOperator)
+{
+	switch (pEvent->event)
+	{
+	case EVENT_WEAPON_SMG1:
+	{
+		Vector vecShootOrigin, vecShootDir;
+		QAngle angDiscard;
+
+		// Support old style attachment point firing
+		if ((pEvent->options == NULL) || (pEvent->options[0] == '\0') || (!pOperator->GetAttachment(pEvent->options, vecShootOrigin, angDiscard)))
+		{
+			vecShootOrigin = pOperator->Weapon_ShootPosition();
+		}
+
+		CAI_BaseNPC *npc = pOperator->MyNPCPointer();
+		ASSERT(npc != NULL);
+		vecShootDir = npc->GetActualShootTrajectory(vecShootOrigin);
+
+		FireNPCPrimaryAttack(pOperator, vecShootOrigin, vecShootDir);
+	}
+	break;
+
+	/*//FIXME: Re-enable
+	case EVENT_WEAPON_AR2_GRENADE:
+	{
+	CAI_BaseNPC *npc = pOperator->MyNPCPointer();
+
+	Vector vecShootOrigin, vecShootDir;
+	vecShootOrigin = pOperator->Weapon_ShootPosition();
+	vecShootDir = npc->GetShootEnemyDir( vecShootOrigin );
+
+	Vector vecThrow = m_vecTossVelocity;
+
+	CGrenadeAR2 *pGrenade = (CGrenadeAR2*)Create( "grenade_ar2", vecShootOrigin, vec3_angle, npc );
+	pGrenade->SetAbsVelocity( vecThrow );
+	pGrenade->SetLocalAngularVelocity( QAngle( 0, 400, 0 ) );
+	pGrenade->SetMoveType( MOVETYPE_FLYGRAVITY );
+	pGrenade->m_hOwner			= npc;
+	pGrenade->m_pMyWeaponAR2	= this;
+	pGrenade->SetDamage(sk_npc_dmg_ar2_grenade.GetFloat());
+
+	// FIXME: arrgg ,this is hard coded into the weapon???
+	m_flNextGrenadeCheck = gpGlobals->curtime + 6;// wait six seconds before even looking again to see if a grenade can be thrown.
+
+	m_iClip2--;
+	}
+	break;
+	*/
+
+	default:
+		BaseClass::Operator_HandleAnimEvent(pEvent, pOperator);
+		break;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Activity
+//-----------------------------------------------------------------------------
+Activity CWeaponC96::GetPrimaryAttackActivity(void)
+{
+	if (m_nShotsFired < 2)
+		return ACT_VM_PRIMARYATTACK;
+
+	if (m_nShotsFired < 3)
+		return ACT_VM_RECOIL1;
+
+	if (m_nShotsFired < 4)
+		return ACT_VM_RECOIL2;
+
+	return ACT_VM_RECOIL3;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool CWeaponC96::Reload(void)
+{
+	bool fRet;
+	//	float fCacheTime = m_flNextSecondaryAttack;
+
+	fRet = DefaultReload(GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD);
+	if (fRet)
+	{
+		// Undo whatever the reload process has done to our secondary
+		// attack timer. We allow you to interrupt reloading to fire
+		// a grenade.
+		//		m_flNextSecondaryAttack = GetOwner()->m_flNextAttack = fCacheTime;
+
+		WeaponSound(RELOAD);
+	}
+
+	return fRet;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponC96::AddViewKick(void)
+{
+#define	EASY_DAMPEN			0.5f
+#define	MAX_VERTICAL_KICK	5.0f	//Degrees
+#define	SLIDE_LIMIT			0.1f	//Seconds
+
+	//Get the view kick
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+
+	if (pPlayer == NULL)
+		return;
+
+	DoMachineGunKick(pPlayer, EASY_DAMPEN, MAX_VERTICAL_KICK, 0.05f, SLIDE_LIMIT);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : flDot - 
+//			flDist - 
+// Output : int
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+const WeaponProficiencyInfo_t *CWeaponC96::GetProficiencyValues()
+{
+	static WeaponProficiencyInfo_t proficiencyTable[] =
+	{
+		{ 7.0, 0.75 },
+		{ 5.00, 0.75 },
+		{ 10.0 / 3.0, 0.75 },
+		{ 5.0 / 3.0, 0.75 },
+		{ 1.00, 1.0 },
+	};
+
+	COMPILE_TIME_ASSERT(ARRAYSIZE(proficiencyTable) == WEAPON_PROFICIENCY_PERFECT + 1);
+
+	return proficiencyTable;
+}
+
