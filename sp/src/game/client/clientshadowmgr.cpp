c3ac061b23fe792431c981ff80c59102f04f0907
FunGamePlays <FunGamePlays@bitbucket.org>
2018-09-01 17:04:58 +0300

    Фиксы env_projectedtexture

--- sp/src/game/client/clientshadowmgr.cpp
+++ sp/src/game/client/clientshadowmgr.cpp
@@ -1290,9 +1290,9 @@ bool CClientShadowMgr::Init()
 
 	SetShadowBlobbyCutoffArea( 0.005 );
 
-	bool bTools = CommandLine()->CheckParm( "-tools" ) != NULL;
-	m_nMaxDepthTextureShadows = bTools ? 4 : 1;	// Just one shadow depth texture in games, more in tools
-
+	//bool bTools = CommandLine()->CheckParm( "-tools" ) != NULL;
+	//m_nMaxDepthTextureShadows = bTools ? 4 : 1;	// Just one shadow depth texture in games, more in tools
+	m_nMaxDepthTextureShadows = 8;
 	bool bLowEnd = ( g_pMaterialSystemHardwareConfig->GetDXSupportLevel() < 80 );
 
 	if ( !bLowEnd && r_shadowrendertotexture.GetBool() )
--- sp/src/game/shared/baseviewmodel_shared.h
+++ sp/src/game/shared/baseviewmodel_shared.h
@@ -147,7 +147,7 @@ public:
 	// Should this object receive shadows?
 	virtual bool			ShouldReceiveProjectedTextures( int flags )
 	{
-		return false;
+		return true;
 	}
 
 	// Add entity to visible view models list?
