0d009e3fa3b8d2a18cbbe001284ad3f1c9cdf8e4
FunGamePlays <FunGamePlays@bitbucket.org>
2018-09-01 21:41:21 +0300

    Добавлена совместимость с Source Shader Editor, а так же улучшены VPC скрипты и батники. Теперь можно без всяких мучений в случае факапа пересоздавать солюшен без траты времени на повторное включение кастомных файлов мода в проект

--- sp/src/game/client/viewrender.cpp
+++ sp/src/game/client/viewrender.cpp
@@ -53,6 +53,7 @@
 #include "clientmode_shared.h"
 #include "sourcevr/isourcevirtualreality.h"
 #include "client_virtualreality.h"
+#include "ShaderEditor/ShaderEditorSystem.h"
 
 #ifdef PORTAL
 //#include "C_Portal_Player.h"
@@ -1359,6 +1360,12 @@ void CViewRender::ViewDrawScene( bool bDrew3dSkybox, SkyboxVisibility_t nSkyboxV
 
 	DrawWorldAndEntities( drawSkybox, view, nClearFlags, pCustomVisibility );
 
+	VisibleFogVolumeInfo_t fogVolumeInfo;
+	render->GetVisibleFogVolume(view.origin, &fogVolumeInfo);
+	WaterRenderInfo_t info;
+	DetermineWaterRenderInfo(fogVolumeInfo, info);
+	g_ShaderEditorSystem->CustomViewRender(&g_CurrentViewID, fogVolumeInfo, info);
+
 	// Disable fog for the rest of the stuff
 	DisableFog();
 
@@ -1985,6 +1992,7 @@ void CViewRender::RenderView( const CViewSetup &view, int nClearFlags, int whatT
 		if ( ( bDrew3dSkybox = pSkyView->Setup( view, &nClearFlags, &nSkyboxVisible ) ) != false )
 		{
 			AddViewToScene( pSkyView );
+			g_ShaderEditorSystem->UpdateSkymask(false, view.x, view.y, view.width, view.height);
 		}
 		SafeRelease( pSkyView );
 
@@ -2042,6 +2050,8 @@ void CViewRender::RenderView( const CViewSetup &view, int nClearFlags, int whatT
 		// Now actually draw the viewmodel
 		DrawViewModels( view, whatToDraw & RENDERVIEW_DRAWVIEWMODEL );
 
+		g_ShaderEditorSystem->UpdateSkymask(bDrew3dSkybox, view.x, view.y, view.width, view.height);
+
 		DrawUnderwaterOverlay();
 
 		PixelVisibility_EndScene();
@@ -2078,6 +2088,8 @@ void CViewRender::RenderView( const CViewSetup &view, int nClearFlags, int whatT
 			}
 			pRenderContext.SafeRelease();
 		}
+
+		g_ShaderEditorSystem->CustomPostRender();
 
 		// And here are the screen-space effects
 
