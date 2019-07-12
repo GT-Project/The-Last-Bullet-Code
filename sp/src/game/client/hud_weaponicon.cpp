a3f70118ed01f8d58d48353171d5c9eb6c50cbcf
FunGamePlays <FunGamePlays@bitbucket.org>
2019-07-12 17:56:26 +0300

    Удалена .vs, изменено имя солюшена и hud_weaponicon(WIP)

---
+++ sp/src/game/client/hud_weaponicon.cpp
@@ -0,0 +1,33 @@
+//KILL ME PLZ
+#include "cbase.h"
+#include "hud.h"
+#include "hudelement.h"
+#include "hud_macros.h"
+#include "hud_numericdisplay.h"
+#include "iclientmode.h"
+#include "iclientvehicle.h"
+#include <vgui_controls/AnimationController.h>
+#include <vgui/ILocalize.h>
+#include <vgui_controls/Panel.h>
+#include "ihudlcd.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CHudWeaponIcon : public CHudElement, public vgui::Panel
+{
+	DECLARE_CLASS_SIMPLE(CHudWeaponIcon, vgui::Panel);
+public:
+	CHudWeaponIcon(const char *pElementName);
+	virtual void				Init(void);
+	virtual void VidInit(void);
+	virtual void Reset(void);
+	virtual void OnThink();
+};
+DECLARE_HUDELEMENT(CHudWeaponIcon);
+CHudWeaponIcon::CHudWeaponIcon(const char *pElementName) : CHudElement(pElementName), BaseClass(NULL, "HudWeaponIcon")
+{
+	vgui::Panel *pParent = g_pClientMode->GetViewport();
+	SetParent(pParent);
+	SetHiddenBits(HIDEHUD_PLAYERDEAD | HIDEHUD_NEEDSUIT);
+}
